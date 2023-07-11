7296101b8669fc1d618dd7716bc3b3bd4a0b7292
TT432 <2437315224@qq.com>
2023-07-11 09:27:33 +0800

    init

---
+++ src/main/java/io/github/tt432/eyelib/Eyelib.java
@@ -0,0 +1,11 @@
+package io.github.tt432.eyelib;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author TT432
+ */
+@Mod(Eyelib.MOD_ID)
+public class Eyelib {
+    public static final String MOD_ID = "eyelib";
+}
