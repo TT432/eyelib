342307d202dda263a4475480013684797343dc51
TT432 <2437315224@qq.com>
2024-03-21 15:00:30 +0800

    :recycle: BrLoopType 修改为 CODEC 实现

--- src/main/java/io/github/tt432/eyelib/client/animation/bedrock/BrLoopType.java
+++ src/main/java/io/github/tt432/eyelib/client/animation/bedrock/BrLoopType.java
@@ -1,27 +1,38 @@
 package io.github.tt432.eyelib.client.animation.bedrock;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
+import com.mojang.datafixers.util.Either;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+import io.github.tt432.eyelib.util.codec.Codecs;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author TT432
  */
+@Slf4j
 public enum BrLoopType {
     HOLD_ON_LAST_FRAME,
     LOOP,
     ONCE;
 
+    public static final Codec<BrLoopType> CODEC = Codec.either(
+                    Codec.STRING.xmap(
+                            s -> switch (s) {
+                                case "hold_on_last_frame" -> HOLD_ON_LAST_FRAME;
+                                case "true" -> LOOP;
+                                default -> ONCE;
+                            },
+                            t -> switch (t) {
+                                case LOOP -> "true";
+                                case HOLD_ON_LAST_FRAME -> "hold_on_last_frame";
+                                default -> "false";
+                            }),
+                    Codec.BOOL.xmap(b -> b ? LOOP : ONCE, t -> t == LOOP))
+            .xmap(Codecs::identity, Either::left)
+            .orElse(ONCE);
+
     public static BrLoopType parse(JsonElement json) {
-        if (json instanceof JsonPrimitive jp) {
-            if (jp.isString() && jp.getAsString().equals("hold_on_last_frame")) {
-                return BrLoopType.HOLD_ON_LAST_FRAME;
-            } else if ((jp.isBoolean() && jp.getAsBoolean()) || (jp.isString() && jp.getAsString().equals("true"))) {
-                return BrLoopType.LOOP;
-            } else {
-                return BrLoopType.ONCE;
-            }
-        } else {
-            return BrLoopType.ONCE;
-        }
+        return CODEC.parse(JsonOps.INSTANCE, json).getOrThrow(true, log::error);
     }
 }
