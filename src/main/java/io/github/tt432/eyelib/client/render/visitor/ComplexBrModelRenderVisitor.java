3410887bb329f9baf28cf55a077db527ec3683af
TT432 <2437315224@qq.com>
2024-05-09 18:21:24 +0800

    :sparkles: 添加 ComplexBrModelRenderVisitor 用于组合 Visitor

---
+++ src/main/java/io/github/tt432/eyelib/client/render/visitor/ComplexBrModelRenderVisitor.java
@@ -0,0 +1,49 @@
+package io.github.tt432.eyelib.client.render.visitor;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import io.github.tt432.eyelib.client.model.bedrock.BrBone;
+import io.github.tt432.eyelib.client.model.bedrock.BrCube;
+import io.github.tt432.eyelib.client.model.bedrock.BrFace;
+import io.github.tt432.eyelib.client.model.bedrock.BrLocator;
+import io.github.tt432.eyelib.client.render.bone.BoneRenderInfoEntry;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author TT432
+ */
+@AllArgsConstructor
+public class ComplexBrModelRenderVisitor extends BrModelRenderVisitor {
+    public final BrModelRenderVisitor visitorA;
+    public final BrModelRenderVisitor visitorB;
+
+    @Override
+    public void setupLight(int light) {
+        visitorA.setupLight(light);
+        visitorB.setupLight(light);
+    }
+
+    @Override
+    public void visitBone(PoseStack poseStack, BrBone bone, BoneRenderInfoEntry boneRenderInfoEntry, VertexConsumer consumer, boolean before) {
+        visitorA.visitBone(poseStack, bone, boneRenderInfoEntry, consumer, before);
+        visitorB.visitBone(poseStack, bone, boneRenderInfoEntry, consumer, before);
+    }
+
+    @Override
+    public void visitCube(PoseStack poseStack, BrCube cube, VertexConsumer consumer) {
+        visitorA.visitCube(poseStack, cube, consumer);
+        visitorB.visitCube(poseStack, cube, consumer);
+    }
+
+    @Override
+    public void visitVertex(PoseStack poseStack, BrCube cube, BrFace face, int vertexId, VertexConsumer consumer) {
+        visitorA.visitVertex(poseStack, cube, face, vertexId, consumer);
+        visitorB.visitVertex(poseStack, cube, face, vertexId, consumer);
+    }
+
+    @Override
+    public void visitLocator(PoseStack poseStack, BrBone bone, String name, BrLocator locator, BoneRenderInfoEntry boneRenderInfoEntry, VertexConsumer consumer) {
+        visitorA.visitLocator(poseStack, bone, name, locator, boneRenderInfoEntry, consumer);
+        visitorB.visitLocator(poseStack, bone, name, locator, boneRenderInfoEntry, consumer);
+    }
+}
