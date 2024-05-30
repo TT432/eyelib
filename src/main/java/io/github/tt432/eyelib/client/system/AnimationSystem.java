a793e6a019a3acc51638b728fe86bcd20346be7d
TT432 <2437315224@qq.com>
2024-05-30 20:13:47 +0800

    :art: 将 system 包移到 client 下

--- src/main/java/io/github/tt432/eyelib/client/system/AnimationSystem.java
+++ src/main/java/io/github/tt432/eyelib/client/system/AnimationSystem.java
@@ -1,4 +1,4 @@
-package io.github.tt432.eyelib.client.animation.system;
+package io.github.tt432.eyelib.client.system;
 
 import io.github.tt432.eyelib.client.animation.bedrock.BrAnimationEntry;
 import io.github.tt432.eyelib.client.animation.bedrock.BrBoneAnimation;
