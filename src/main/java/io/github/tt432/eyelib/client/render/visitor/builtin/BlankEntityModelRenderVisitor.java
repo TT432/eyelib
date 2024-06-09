03aac3dd44bd592fb2040773d6dfa653918adab3
TT432 <2437315224@qq.com>
2024-06-09 18:00:01 +0800

    :recycle: 将渲染中一部分参数抽出来成为 RenderParams

--- src/main/java/io/github/tt432/eyelib/client/render/visitor/builtin/BlankEntityModelRenderVisitor.java
+++ src/main/java/io/github/tt432/eyelib/client/render/visitor/builtin/BlankEntityModelRenderVisitor.java
@@ -4,11 +4,9 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import io.github.tt432.eyelib.client.model.bedrock.BrCube;
 import io.github.tt432.eyelib.client.model.bedrock.BrFace;
+import io.github.tt432.eyelib.client.render.RenderParams;
 import lombok.Setter;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.world.entity.Entity;
-import org.jetbrains.annotations.Nullable;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
@@ -21,24 +19,19 @@ public class BlankEntityModelRenderVisitor extends ModelRenderVisitor {
     private static final Vector4f tPosition = new Vector4f();
     private static final Vector3f tNormal = new Vector3f();
 
-    int light;
-
     @Override
-    public void setupLight(int light) {
-        this.light = light;
-    }
-
-    @Override
-    public void visitVertex(@Nullable Entity renderTarget, PoseStack poseStack, BrCube cube, RenderType renderType, BrFace face, int vertexId, VertexConsumer consumer) {
+    public void visitVertex(RenderParams renderParams, BrCube cube, BrFace face, int vertexId) {
         Vector3f normal = face.getNormal();
         Vector3f vertex = face.getVertex()[vertexId];
         Vector2f uv = face.getUv()[vertexId];
-
+        PoseStack poseStack = renderParams.poseStack();
         PoseStack.Pose last = poseStack.last();
 
         last.pose().transformAffine(vertex.x, vertex.y, vertex.z, 1, tPosition);
         last.normal().transform(normal, tNormal);
 
+        VertexConsumer consumer = renderParams.consumer();
+        int light = renderParams.light();
         consumer.vertex(tPosition.x, tPosition.y, tPosition.z,
                 1, 1, 1, 1,
                 uv.x, uv.y,
