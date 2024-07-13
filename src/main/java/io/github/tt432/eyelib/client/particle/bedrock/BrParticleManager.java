dc2ec2d0556698abb4aa6e18846f72d02f228dd5
TT432 <2437315224@qq.com>
2024-07-13 16:45:12 +0800

    :recycle: 对 emitter 添加 string 以便移除

--- src/main/java/io/github/tt432/eyelib/client/particle/bedrock/BrParticleManager.java
+++ src/main/java/io/github/tt432/eyelib/client/particle/bedrock/BrParticleManager.java
@@ -1,15 +1,10 @@
 package io.github.tt432.eyelib.client.particle.bedrock;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import io.github.tt432.eyelib.Eyelib;
-import io.github.tt432.eyelib.capability.RenderData;
-import io.github.tt432.eyelib.client.loader.BrParticleLoader;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -17,22 +12,23 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
-import org.joml.Vector3f;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author TT432
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BrParticleManager {
-    private static final List<BrParticleEmitter> emitters = Collections.synchronizedList(new ArrayList<>());
+    private static final Map<String, BrParticleEmitter> emitters = Collections.synchronizedMap(new HashMap<>());
     private static final List<BrParticleParticle> particles = Collections.synchronizedList(new ArrayList<>());
 
-    public static void spawnEmitter(final BrParticleEmitter emitter) {
-        emitters.add(emitter);
+    public static void spawnEmitter(final String id, final BrParticleEmitter emitter) {
+        emitters.put(id, emitter);
+    }
+
+    public static void removeEmitter(final String id) {
+        emitters.remove(id);
     }
 
     public static void spawnParticle(final BrParticleParticle particle) {
@@ -50,11 +46,11 @@ public class BrParticleManager {
 
                     synchronized (instance) {
                         if (instance.level != null) {
-                            emitters.forEach(e -> e.getTimer().setPaused(instance.isPaused()));
+                            emitters.forEach((k, e) -> e.getTimer().setPaused(instance.isPaused()));
                             particles.forEach(e -> e.getTimer().setPaused(instance.isPaused()));
 
-                            emitters.removeIf(BrParticleEmitter::isRemoved);
-                            emitters.forEach(BrParticleEmitter::onRenderFrame);
+                            emitters.entrySet().removeIf(e -> e.getValue().isRemoved());
+                            emitters.values().forEach(BrParticleEmitter::onRenderFrame);
                             particles.removeIf(BrParticleParticle::isRemoved);
                             particles.forEach(BrParticleParticle::onRenderFrame);
                         }
@@ -69,7 +65,7 @@ public class BrParticleManager {
     public static final class ForgeEvents {
         @SubscribeEvent
         public static void onEvent(ClientTickEvent.Pre event) {
-            emitters.forEach(BrParticleEmitter::onTick);
+            emitters.values().forEach(BrParticleEmitter::onTick);
         }
 
         @SubscribeEvent
@@ -89,17 +85,6 @@ public class BrParticleManager {
         public static void onEvent(ClientPlayerNetworkEvent.LoggingOut event) {
             emitters.clear();
             particles.clear();
-        }
-
-        @SubscribeEvent
-        public static void onEvent(ClientPlayerNetworkEvent.LoggingIn event) {
-            LocalPlayer player = event.getPlayer();
-            emitters.add(new BrParticleEmitter(
-                    BrParticleLoader.getParticle(ResourceLocation.fromNamespaceAndPath(Eyelib.MOD_ID, "rainbow")),
-                    RenderData.getComponent(player).getScope(),
-                    player.level(),
-                    new Vector3f()
-            ));
         }
     }
 }
