069eff5760ba5ac59ac9cec7947b03afc9f7b80b
TT432 <2437315224@qq.com>
2024-10-01 17:07:59 +0800

    :fire: 删除 ReplacedTooltip 系统

--- src/main/java/io/github/tt432/eyelib/Eyelib.java
+++ src/main/java/io/github/tt432/eyelib/Eyelib.java
@@ -1,7 +1,6 @@
 package io.github.tt432.eyelib;
 
 import io.github.tt432.eyelib.capability.EyelibAttachableData;
-import io.github.tt432.eyelib.capability.item.EyelibDataComponents;
 import io.github.tt432.eyelib.client.render.visitor.BuiltInBrModelRenderVisitors;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -15,7 +14,6 @@ public class Eyelib {
 
     public Eyelib(IEventBus bus) {
         EyelibAttachableData.ATTACHMENT_TYPES.register(bus);
-        EyelibDataComponents.COMPONENTS.register(bus);
         BuiltInBrModelRenderVisitors.VISITORS.register(bus);
     }
 }
