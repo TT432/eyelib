0118a3edc9eb84aa88f2522616935a7caef6901c
DavidWang19 <dwang0340@gmail.com>
2025-09-12 16:12:12 +0100

    delombok

--- src/main/java/io/github/tt432/eyelib/molang/compiler/MolangClassDescs.java
+++ src/main/java/io/github/tt432/eyelib/molang/compiler/MolangClassDescs.java
@@ -2,15 +2,17 @@ package io.github.tt432.eyelib.molang.compiler;
 
 import io.github.tt432.eyelib.molang.MolangScope;
 import io.github.tt432.eyelib.molang.type.MolangObject;
-import lombok.experimental.UtilityClass;
 
 import java.lang.constant.ClassDesc;
 
 /**
  * @author TT432
  */
-@UtilityClass
-public class MolangClassDescs {
-    public static final ClassDesc CD_MolangObject = ClassDesc.of(MolangObject.class.getName());
-    public static final ClassDesc CD_MolangScope =ClassDesc.of(MolangScope .class.getName());
+public final class MolangClassDescs {
+    public static ClassDesc CD_MolangObject = ClassDesc.of(MolangObject.class.getName());
+    public static ClassDesc CD_MolangScope = ClassDesc.of(MolangScope.class.getName());
+
+    private MolangClassDescs() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
 }
