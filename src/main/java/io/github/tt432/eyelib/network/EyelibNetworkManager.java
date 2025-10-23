e78dbd06e90d6e89d39ee843ceb9eadcfce6416c
TT432 <2437315224@qq.com>
2025-10-23 22:24:25 +0800

    修复在服务端错误地加载客户端类的问题

--- src/main/java/io/github/tt432/eyelib/network/EyelibNetworkManager.java
+++ src/main/java/io/github/tt432/eyelib/network/EyelibNetworkManager.java
@@ -2,19 +2,14 @@ package io.github.tt432.eyelib.network;
 
 import io.github.tt432.eyelib.Eyelib;
 import io.github.tt432.eyelib.capability.EyelibAttachableData;
-import io.github.tt432.eyelib.capability.RenderData;
-import io.github.tt432.eyelib.capability.component.ModelComponent;
-import io.github.tt432.eyelib.client.loader.BrParticleLoader;
-import io.github.tt432.eyelib.client.particle.bedrock.BrParticle;
-import io.github.tt432.eyelib.client.particle.bedrock.BrParticleEmitter;
-import io.github.tt432.eyelib.client.particle.bedrock.BrParticleRenderManager;
 import io.github.tt432.eyelib.util.data_attach.DataAttachmentHelper;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.NetworkRegistry;
@@ -55,43 +50,43 @@ public class EyelibNetworkManager {
         INSTANCE.messageBuilder(ModelComponentSyncPacket.class, nextId())
                 .encoder(ModelComponentSyncPacket.STREAM_CODEC::encode)
                 .decoder(ModelComponentSyncPacket.STREAM_CODEC::decode)
-                .consumerMainThread(onClientHandle(EyelibNetworkManager::onModelComponentSyncPacket))
+                .consumerMainThread(EyelibNetworkManager::onModelComponentSyncPacket)
                 .add();
 
         INSTANCE.messageBuilder(AnimationComponentSyncPacket.class, nextId())
                 .encoder(AnimationComponentSyncPacket.STREAM_CODEC::encode)
                 .decoder(AnimationComponentSyncPacket.STREAM_CODEC::decode)
-                .consumerMainThread(onClientHandle(EyelibNetworkManager::onAnimationComponentSyncPacket))
+                .consumerMainThread(EyelibNetworkManager::onAnimationComponentSyncPacket)
                 .add();
 
         INSTANCE.messageBuilder(RemoveParticlePacket.class, nextId())
                 .encoder(RemoveParticlePacket.STREAM_CODEC::encode)
                 .decoder(RemoveParticlePacket.STREAM_CODEC::decode)
-                .consumerMainThread(onClientHandle(EyelibNetworkManager::onRemoveParticlePacket))
+                .consumerMainThread(EyelibNetworkManager::onRemoveParticlePacket)
                 .add();
 
         INSTANCE.messageBuilder(SpawnParticlePacket.class, nextId())
                 .encoder(SpawnParticlePacket.STREAM_CODEC::encode)
                 .decoder(SpawnParticlePacket.STREAM_CODEC::decode)
-                .consumerMainThread(onClientHandle(EyelibNetworkManager::onSpawnParticlePacket))
+                .consumerMainThread(EyelibNetworkManager::onSpawnParticlePacket)
                 .add();
 
         INSTANCE.messageBuilder(ExtraEntityUpdateDataPacket.class, nextId())
                 .encoder(ExtraEntityUpdateDataPacket.STREAM_CODEC::encode)
                 .decoder(ExtraEntityUpdateDataPacket.STREAM_CODEC::decode)
-                .consumerMainThread(onClientHandle(EyelibNetworkManager::onExtraEntityUpdateDataPacket))
+                .consumerMainThread(EyelibNetworkManager::onExtraEntityUpdateDataPacket)
                 .add();
 
         INSTANCE.messageBuilder(UniDataUpdatePacket.class, nextId())
                 .encoder(UniDataUpdatePacket.STREAM_CODEC::encode)
                 .decoder(UniDataUpdatePacket.STREAM_CODEC::decode)
-                .consumerMainThread(onClientHandle(EyelibNetworkManager::onUniDataUpdatePacket))
+                .consumerMainThread(EyelibNetworkManager::onUniDataUpdatePacket)
                 .add();
 
         INSTANCE.messageBuilder(ExtraEntityDataPacket.class, nextId())
                 .encoder(ExtraEntityDataPacket.STREAM_CODEC::encode)
                 .decoder(ExtraEntityDataPacket.STREAM_CODEC::decode)
-                .consumerMainThread(onClientHandle(EyelibNetworkManager::onExtraEntityDataPacket))
+                .consumerMainThread(EyelibNetworkManager::onExtraEntityDataPacket)
                 .add();
     }
 
@@ -108,14 +103,54 @@ public class EyelibNetworkManager {
     }
 
     // <editor-fold desc="Handlers">
+    public static void onModelComponentSyncPacket(ModelComponentSyncPacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() ->
+                // Make sure it's only executed on the physical client
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> NetClientHandlers.onModelComponentSyncPacket(msg, ctx.get()))
+        );
+        ctx.get().setPacketHandled(true);
+    }
 
-    private static <T> BiConsumer<T, Supplier<NetworkEvent.Context>> onClientHandle(BiConsumer<T, NetworkEvent.Context> consumer) {
-        return (packet, supplier) -> {
-            var context = supplier.get();
-            if (context.getDirection().getReceptionSide().isClient()) {
-                consumer.accept(packet, context);
-            }
-        };
+    public static void onAnimationComponentSyncPacket(AnimationComponentSyncPacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() ->
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> NetClientHandlers.onAnimationComponentSyncPacket(msg, ctx.get()))
+        );
+        ctx.get().setPacketHandled(true);
+    }
+
+    public static void onRemoveParticlePacket(RemoveParticlePacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() ->
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> NetClientHandlers.onRemoveParticlePacket(msg, ctx.get()))
+        );
+        ctx.get().setPacketHandled(true);
+    }
+
+    public static void onSpawnParticlePacket(SpawnParticlePacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() ->
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> NetClientHandlers.onSpawnParticlePacket(msg, ctx.get()))
+        );
+        ctx.get().setPacketHandled(true);
+    }
+
+    public static void onExtraEntityUpdateDataPacket(ExtraEntityUpdateDataPacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() ->
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> NetClientHandlers.onExtraEntityUpdateDataPacket(msg, ctx.get()))
+        );
+        ctx.get().setPacketHandled(true);
+    }
+
+    public static void onUniDataUpdatePacket(UniDataUpdatePacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() ->
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> NetClientHandlers.onUniDataUpdatePacket(msg, ctx.get()))
+        );
+        ctx.get().setPacketHandled(true);
+    }
+
+    public static void onExtraEntityDataPacket(ExtraEntityDataPacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() ->
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> NetClientHandlers.onExtraEntityDataPacket(msg, ctx.get()))
+        );
+        ctx.get().setPacketHandled(true);
     }
 
     private static <T> BiConsumer<T, Supplier<NetworkEvent.Context>> onServerHandle(BiConsumer<T, NetworkEvent.Context> consumer) {
@@ -143,87 +178,6 @@ public class EyelibNetworkManager {
 
     // </editor-fold>
 
-    // <editor-fold desc="Client handlers">
-
-    private static void onModelComponentSyncPacket(ModelComponentSyncPacket packet, NetworkEvent.Context context) {
-        if (Minecraft.getInstance().level == null) {
-            return;
-        }
-        Entity entity = Minecraft.getInstance().level.getEntity(packet.entityId());
-        if (entity == null) {
-            return;
-        }
-        RenderData<?> data = RenderData.getComponent(entity);
-        if (data == null) {
-            return;
-        }
-        data.getModelComponents().clear();
-        for (ModelComponent.SerializableInfo serializableInfo : packet.modelInfo()) {
-            ModelComponent e = new ModelComponent();
-            e.setInfo(serializableInfo);
-            data.getModelComponents().add(e);
-        }
-    }
-
-    private static void onAnimationComponentSyncPacket(AnimationComponentSyncPacket packet, NetworkEvent.Context context) {
-        if (Minecraft.getInstance().level == null) {
-            return;
-        }
-        Entity entity = Minecraft.getInstance().level.getEntity(packet.entityId());
-        if (entity == null) {
-            return;
-        }
-        RenderData<?> data = RenderData.getComponent(entity);
-        var info = packet.animationInfo();
-        data.getAnimationComponent().setInfo(info);
-    }
-
-    private static void onRemoveParticlePacket(RemoveParticlePacket packet, NetworkEvent.Context context) {
-        BrParticleRenderManager.removeEmitter(packet.removeId());
-    }
-
-    private static void onSpawnParticlePacket(SpawnParticlePacket packet, NetworkEvent.Context context) {
-        BrParticle particle = BrParticleLoader.getParticle(packet.particleId());
-        if (particle != null) {
-            var data = DataAttachmentHelper.getOrCreate(EyelibAttachableData.RENDER_DATA.get(), Minecraft.getInstance().player);
-            BrParticleRenderManager.spawnEmitter(
-                    packet.spawnId(),
-                    new BrParticleEmitter(
-                            particle,
-                            data.getScope(),
-                            // 防止服务端加载 ClientLevel
-                            Minecraft.getInstance().level,
-                            packet.position()
-                    )
-            );
-        }
-    }
-
-    private static void onExtraEntityUpdateDataPacket(ExtraEntityUpdateDataPacket packet, NetworkEvent.Context context) {
-        if (Minecraft.getInstance().level == null) {
-            return;
-        }
-        var entity = Minecraft.getInstance().level.getEntity(packet.entityId());
-        DataAttachmentHelper.set(EyelibAttachableData.EXTRA_ENTITY_UPDATE.get(), entity, packet.data());
-    }
-
-    private static <T> void onUniDataUpdatePacket(UniDataUpdatePacket<T> packet, NetworkEvent.Context context) {
-        if (Minecraft.getInstance().level == null) {
-            return;
-        }
-        var entity = Minecraft.getInstance().level.getEntity(packet.entityId());
-        DataAttachmentHelper.set(packet.type(), entity, packet.data());
-    }
-
-    private static void onExtraEntityDataPacket(ExtraEntityDataPacket packet, NetworkEvent.Context context) {
-        if (Minecraft.getInstance().level == null) {
-            return;
-        }
-        var entity = Minecraft.getInstance().level.getEntity(packet.entityId());
-        DataAttachmentHelper.set(EyelibAttachableData.EXTRA_ENTITY_DATA.get(), entity, packet.data());
-    }
-
-    // </editor-fold>
 
     // </editor-fold>
 }
---
+++ src/main/java/io/github/tt432/eyelib/network/NetClientHandlers.java
@@ -0,0 +1,101 @@
+package io.github.tt432.eyelib.network;
+
+import io.github.tt432.eyelib.capability.EyelibAttachableData;
+import io.github.tt432.eyelib.capability.RenderData;
+import io.github.tt432.eyelib.capability.component.ModelComponent;
+import io.github.tt432.eyelib.client.loader.BrParticleLoader;
+import io.github.tt432.eyelib.client.particle.bedrock.BrParticle;
+import io.github.tt432.eyelib.client.particle.bedrock.BrParticleEmitter;
+import io.github.tt432.eyelib.client.particle.bedrock.BrParticleRenderManager;
+import io.github.tt432.eyelib.util.data_attach.DataAttachmentHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.network.NetworkEvent;
+
+/**
+ * @author TT432
+ */
+public class NetClientHandlers {
+    // <editor-fold desc="Client handlers">
+
+    public static void onModelComponentSyncPacket(ModelComponentSyncPacket packet, NetworkEvent.Context context) {
+        if (Minecraft.getInstance().level == null) {
+            return;
+        }
+        Entity entity = Minecraft.getInstance().level.getEntity(packet.entityId());
+        if (entity == null) {
+            return;
+        }
+        RenderData<?> data = RenderData.getComponent(entity);
+        if (data == null) {
+            return;
+        }
+        data.getModelComponents().clear();
+        for (ModelComponent.SerializableInfo serializableInfo : packet.modelInfo()) {
+            ModelComponent e = new ModelComponent();
+            e.setInfo(serializableInfo);
+            data.getModelComponents().add(e);
+        }
+    }
+
+    public static void onAnimationComponentSyncPacket(AnimationComponentSyncPacket packet, NetworkEvent.Context context) {
+        if (Minecraft.getInstance().level == null) {
+            return;
+        }
+        Entity entity = Minecraft.getInstance().level.getEntity(packet.entityId());
+        if (entity == null) {
+            return;
+        }
+        RenderData<?> data = RenderData.getComponent(entity);
+        var info = packet.animationInfo();
+        data.getAnimationComponent().setInfo(info);
+    }
+
+    public static void onRemoveParticlePacket(RemoveParticlePacket packet, NetworkEvent.Context context) {
+        BrParticleRenderManager.removeEmitter(packet.removeId());
+    }
+
+    public static void onSpawnParticlePacket(SpawnParticlePacket packet, NetworkEvent.Context context) {
+        BrParticle particle = BrParticleLoader.getParticle(packet.particleId());
+        if (particle != null) {
+            var data = DataAttachmentHelper.getOrCreate(EyelibAttachableData.RENDER_DATA.get(), Minecraft.getInstance().player);
+            BrParticleRenderManager.spawnEmitter(
+                    packet.spawnId(),
+                    new BrParticleEmitter(
+                            particle,
+                            data.getScope(),
+                            // 防止服务端加载 ClientLevel
+                            Minecraft.getInstance().level,
+                            packet.position()
+                    )
+            );
+        }
+    }
+
+    public static void onExtraEntityUpdateDataPacket(ExtraEntityUpdateDataPacket packet, NetworkEvent.Context context) {
+        if (Minecraft.getInstance().level == null) {
+            return;
+        }
+        var entity = Minecraft.getInstance().level.getEntity(packet.entityId());
+        DataAttachmentHelper.set(EyelibAttachableData.EXTRA_ENTITY_UPDATE.get(), entity, packet.data());
+    }
+
+    public static <T> void onUniDataUpdatePacket(UniDataUpdatePacket<T> packet, NetworkEvent.Context context) {
+        if (Minecraft.getInstance().level == null) {
+            return;
+        }
+        var entity = Minecraft.getInstance().level.getEntity(packet.entityId());
+        DataAttachmentHelper.set(packet.type(), entity, packet.data());
+    }
+
+    public static void onExtraEntityDataPacket(ExtraEntityDataPacket packet, NetworkEvent.Context context) {
+        if (Minecraft.getInstance().level == null) {
+            return;
+        }
+        var entity = Minecraft.getInstance().level.getEntity(packet.entityId());
+        DataAttachmentHelper.set(EyelibAttachableData.EXTRA_ENTITY_DATA.get(), entity, packet.data());
+    }
+
+    // </editor-fold>
+
+}
