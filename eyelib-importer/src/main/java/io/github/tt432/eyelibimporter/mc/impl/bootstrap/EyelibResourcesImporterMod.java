a85d413ca9beaf9c892bea7dcb0b0c7a0d485721
TT432 <2437315224@qq.com>
2026-04-11 19:38:17 +0800

    build: rename module identities for importer and molang
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ eyelib-importer/src/main/java/io/github/tt432/eyelibimporter/mc/impl/bootstrap/EyelibResourcesImporterMod.java
@@ -0,0 +1,8 @@
+package io.github.tt432.eyelibimporter.mc.impl.bootstrap;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EyelibResourcesImporterMod.MOD_ID)
+public class EyelibResourcesImporterMod {
+    public static final String MOD_ID = "eyelibimporter";
+}
---
+++ eyelib-molang/src/main/java/io/github/tt432/eyelibmolang/mc/impl/bootstrap/EyelibMolangMod.java
@@ -0,0 +1,8 @@
+package io.github.tt432.eyelibmolang.mc.impl.bootstrap;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EyelibMolangMod.MOD_ID)
+public class EyelibMolangMod {
+    public static final String MOD_ID = "eyelibmolang";
+}
